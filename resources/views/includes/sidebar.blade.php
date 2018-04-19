09585dd7ec49e2a553a8b0f0fd6050ac830e8066
ranitachi <fachran.nazarullah@gmail.com>
2018-04-19 10:05:15 +0700

    update sidebar menu

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -10,91 +10,34 @@
                             <li class="title">MAIN</li>
                             <li><a href="index.html"><span class="nav-icon-hexa">Ds</span> Dashboard</a></li>
                                                    
-                            <li><a href="layouts-examples.html"><span class="nav-icon-hexa">Mp</span> Manajemen Pelatihan</a></li>
+                            <li>
+                                <a href="#"><span class="nav-icon-hexa">Mp</span> Manajemen Pelatihan</a>
+                                <ul>                                
+                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Kp</span> Kategori Pelatihan</a></li>
+                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Jp</span> Jenis Pelatihan</a></li>
+                                </ul>
+                            </li>
                             <li><a href="landing-page.html" target="_blank"><span class="nav-icon-hexa">Mu</span> Manajemen User</a></li>
                             <li><a href="documentation.html"><span class="nav-icon-hexa">Mj</span> Manajemen Jadwal</a></li>
                             <li>
                                 <a href="#"><span class="nav-icon-hexa">Md</span> Master Data</a>
                                 <ul>                                
-                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Fq</span> FAQ</a></li>
-                                    <li><a href="pages-gallery.html"><span class="nav-icon-hexa">Ga</span> Gallery</a></li>                
-                                    <li><a href="pages-search.html"><span class="nav-icon-hexa">Sr</span> Search Result</a></li>
-                                    <li><a href="pages-contact-list.html"><span class="nav-icon-hexa">Cl</span> Contact List</a></li>
-                                    <li><a href="pages-calendar.html"><span class="nav-icon-hexa">Cr</span> Calendar</a></li>
-                                    <li><a href="pages-404.html"><span class="nav-icon-hexa">Er</span> Error 404</a></li>
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Em</span> Email</a>
-                                        <ul>                
-                                            <li><a href="pages-email-inbox.html"><span class="nav-icon-hexa">Ib</span> Inbox</a></li>
-                                            <li><a href="pages-email-message.html"><span class="nav-icon-hexa">Ms</span> Message</a></li>
-                                            <li><a href="pages-email-compose.html"><span class="nav-icon-hexa">Cp</span> Compose</a></li>
-                                            <li><a href="pages-email-templates.html"><span class="nav-icon-hexa">Tp</span> Templates</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Pt</span> Payment</a>
-                                        <ul>                
-                                            <li><a href="pages-payment-invoice.html"><span class="nav-icon-hexa">Pi</span> Invoice</a></li>                        
-                                            <li><a href="pages-payment-pricing.html"><span class="nav-icon-hexa">Pt</span> Pricing Tables</a></li>                        
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Cs</span> Call Service</a>
-                                        <ul>                
-                                            <li><a href="pages-call-service-daily.html"><span class="nav-icon-hexa">Ds</span> Daily Statistics</a></li>                        
-                                            <li><a href="pages-call-service-process.html"><span class="nav-icon-hexa">Pw</span> Process Window</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Ba</span> Bank Application</a>
-                                        <ul>                
-                                            <li><a href="pages-bank-main.html"><span class="nav-icon-hexa">Mn</span> Main</a></li>
-                                            <li><a href="pages-bank-cards.html"><span class="nav-icon-hexa">Cs</span> My Cards</a></li>
-                                            <li><a href="pages-bank-deposits.html"><span class="nav-icon-hexa">Dp</span> Deposits</a></li>
-                                            <li><a href="pages-bank-activity.html"><span class="nav-icon-hexa">Ac</span> Activity</a></li>
-                                            <li><a href="pages-bank-settings.html"><span class="nav-icon-hexa">St</span> Settings</a></li>
-                                            <li><a href="pages-bank-security.html"><span class="nav-icon-hexa">Sc</span> Security</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Bp</span> Blog Pages</a>
-                                        <ul>                
-                                            <li><a href="pages-blog-main.html"><span class="nav-icon-hexa">Ma</span> Main (Variant 1)</a></li>
-                                            <li><a href="pages-blog-main-2.html"><span class="nav-icon-hexa">Mn</span> Main (Variant 2)</a></li>
-                                            <li><a href="pages-blog-category.html"><span class="nav-icon-hexa">Ct</span> Category (Right Sidebar)</a></li>
-                                            <li><a href="pages-blog-category-2.html"><span class="nav-icon-hexa">Cr</span> Category (Left Sidebar)</a></li>
-                                            <li><a href="pages-blog-single.html"><span class="nav-icon-hexa">Sn</span> Single</a></li>                        
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Pf</span> User Profiles</a>
-                                        <ul>
-                                            <li><a href="pages-profile-social.html"><span class="nav-icon-hexa">Sp</span> Social Profile</a></li>
-                                            <li><a href="pages-profile-card.html"><span class="nav-icon-hexa">Pc</span> Profile Card</a></li>                
-                                        </ul>
-                                    </li>
-                                    <li><a href="pages-help.html"><span class="nav-icon-hexa">Hp</span> Help</a></li>                
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Ms</span> Messages</a>
-                                        <ul>
-                                            <li><a href="pages-messages-chat.html"><span class="nav-icon-hexa">Ct</span> Chat</a></li>
-                                            <li><a href="pages-messages-list.html"><span class="nav-icon-hexa">Ml</span> Messages List</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="pages-lock-screen.html"><span class="nav-icon-hexa">Ls</span> Lock Screen</a></li>
-                                    <li>
-                                        <a href="#"><span class="nav-icon-hexa">Si</span> Log In / Sign In</a>
-                                        <ul>
-                                            <li><a href="pages-login.html"><span class="nav-icon-hexa">Li</span> Log In</a></li>
-                                            <li><a href="pages-login-bg.html"><span class="nav-icon-hexa">Lb</span> Log In (Background)</a></li>
-                                            <li><a href="pages-signin.html"><span class="nav-icon-hexa">Si</span> Sign In</a></li>
-                                            <li><a href="pages-signin-bg.html"><span class="nav-icon-hexa">Sb</span> Sign In (Background)</a></li>
-                                        </ul>
-                                    </li>
+                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Dk</span> Data Karyawan</a></li>
+                                    <li><a href="pages-gallery.html"><span class="nav-icon-hexa">Di</span> Data Instruktur</a></li>                
+                                    <li><a href="pages-search.html"><span class="nav-icon-hexa">Dp</span> Data Perusahaan </a></li>
+                                    <li><a href="pages-contact-list.html"><span class="nav-icon-hexa">Dq</span> Data Questioner</a></li>
+                                    
                                 </ul>
                             </li> 
                             <li class="title">PENGATURAN</li>                
-                            <li><a href="#"><span class="nav-icon-hexa">Ps</span> Pengaturan Sistem</a></li>
+                            <li><a href="#"><span class="nav-icon-hexa">Ps</span> Pengaturan Sistem</a>
+                                <ul>                                
+                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Pr</span> Data Provinsi</a></li>
+                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Kb</span> Data Kabupaten/Kota</a></li>
+                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Kc</span> Data Kecamatan</a></li>
+                                    <li><a href="pages-faq.html"><span class="nav-icon-hexa">Kl</span> Data Kelurahan</a></li>
+                                </ul>
+                            </li>
                            
                         </ul>
                     </nav>
