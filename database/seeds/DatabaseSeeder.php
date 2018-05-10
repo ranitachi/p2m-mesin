b7a1f4fe2be5a5ec9cff2b3ea7b24b91a886b8b9
root <root@abrisamedia.com>
2018-05-10 17:03:15 -0400

    update server

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTabel::class);
+        //$this->call(UserTabel::class);
         // $this->call(Datadirektur::class);
         Eloquent::unguard();
 
