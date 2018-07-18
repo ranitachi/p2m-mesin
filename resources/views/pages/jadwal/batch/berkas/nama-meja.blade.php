1b16e2a2712f72bb8e62a60ce189ece3e741a151
ranitachi <fachran.nazarullah@gmail.com>
2018-07-18 15:08:51 +0700

    update

--- resources/views/pages/jadwal/batch/berkas/nama-meja.blade.php
+++ resources/views/pages/jadwal/batch/berkas/nama-meja.blade.php
@@ -15,11 +15,11 @@
                         <table border="0" style="width:100%;margin-left:0px !important;border:3px solid #888;;margin-right:0px;margin-bottom:20px;margin-top:0px;" cellpadding="0" cellspacing="0">
 							<tr>
 								<td style="text-align:center;vertical-align:top;height:20mm">
-                                    <div style="font-size:70px !important;padding:90px 0px;border-bottom:3px solid lightblue;font-family:readventor !important;white-space: nowrap;">{{strtoupper($item->peserta->nama_lengkap)}}</div>
+                                    <div style="font-size:70px !important;padding:85px 0px;border-bottom:3px solid lightblue;font-family:readventor !important;white-space: nowrap;">{{strtoupper($item->peserta->nama_lengkap)}}</div>
 								</td>
 							</tr>
 							<tr>
-								<td style="text-align:center;padding:20px 0;height:10mm">
+								<td style="text-align:center;padding:15px 0;height:10mm">
 									@if (isset($item->peserta->perusahaan->nama_perusahaan))
 									
 										<h3 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-size:25px">{{strtoupper($item->peserta->perusahaan->nama_perusahaan)}}</h3>
