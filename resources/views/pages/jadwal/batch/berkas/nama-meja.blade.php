d5bf8156b47654ac5d2393f4abc9d190962f7576
ranitachi <fachran.nazarullah@gmail.com>
2018-07-18 15:08:04 +0700

    update

--- resources/views/pages/jadwal/batch/berkas/nama-meja.blade.php
+++ resources/views/pages/jadwal/batch/berkas/nama-meja.blade.php
@@ -12,14 +12,14 @@
 				@endphp
 				@foreach ($peserta as $idx=>$item)
 					
-                        <table border="0" style="width:100%;margin-left:0px !important;border:3px solid #888;;margin-right:0px;margin-bottom:50px;margin-top:10px;" cellpadding="0" cellspacing="0">
+                        <table border="0" style="width:100%;margin-left:0px !important;border:3px solid #888;;margin-right:0px;margin-bottom:20px;margin-top:0px;" cellpadding="0" cellspacing="0">
 							<tr>
-								<td style="text-align:center;vertical-align:top;height:10mm">
-                                    <div style="font-size:70px !important;padding:70px 0px;border-bottom:3px solid lightblue;font-family:readventor !important;white-space: nowrap;">{{strtoupper($item->peserta->nama_lengkap)}}</div>
+								<td style="text-align:center;vertical-align:top;height:20mm">
+                                    <div style="font-size:70px !important;padding:90px 0px;border-bottom:3px solid lightblue;font-family:readventor !important;white-space: nowrap;">{{strtoupper($item->peserta->nama_lengkap)}}</div>
 								</td>
 							</tr>
 							<tr>
-								<td style="text-align:center;padding:10px 0;height:10mm">
+								<td style="text-align:center;padding:20px 0;height:10mm">
 									@if (isset($item->peserta->perusahaan->nama_perusahaan))
 									
 										<h3 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-size:25px">{{strtoupper($item->peserta->perusahaan->nama_perusahaan)}}</h3>
